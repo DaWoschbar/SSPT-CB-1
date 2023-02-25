6374a622aef4d227277a06e7eaa2a756ae0eb726
Roman Schabus <54069085+DaWoschbar@users.noreply.github.com>
2023-02-25 13:15:35 +0100

    Finished Uebung 1

---
+++ Uebung1/Uebung1/Uebung1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Uebung1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 3;
+            Console.WriteLine($"Value var x: {x} \nValue var y: {y}");
+            Console.WriteLine($"Sum: {x + y}");
+            Console.WriteLine($"Div: {x / y}");
+            Console.WriteLine($"As string: {x}{y}");
+        }
+    }
+}
