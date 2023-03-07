afbde61c346e6010f485a2451b1c7031b284c2d9
Roman Schabus <54069085+DaWoschbar@users.noreply.github.com>
2023-03-07 15:47:26 +0100

    finished first room

--- Textadventure/Textadventure/Program.cs
+++ Textadventure/Textadventure/Program.cs
@@ -1,25 +1,29 @@
-﻿namespace Textadventure
+﻿using System.Data;
+using System.Runtime.InteropServices;
+
+namespace Textadventure
 {
     internal class Program
     {
+        //fields needed to check if the input is valid
+        private static List<String> allowedCommands = new List<String>() { "n", "e", "w", "s", "u", "x", "a", "clear" };
+
+        //value defines the current room starting at 1 for better understanding
+        private static int currentRoomNumber = 1;
         static void Main(string[] args)
         {
+
             PrintIntroduction();
             PrintHelp();
-            PrintRoomDescription1();
+            Room1();
 
-            while (true) 
-            { 
-
-            }
-
-            
         }
 
         private static void PrintHelp()
         {
             Console.WriteLine("\n");
             Console.WriteLine("a\t... describe the current room");
+            Console.WriteLine("u [item]\t ... use an item");
             Console.WriteLine("n\t... go north");
             Console.WriteLine("w\t... go west");
             Console.WriteLine("e\t... to east");
@@ -32,33 +36,82 @@
         {
             Console.WriteLine("The Quest for the Golden Key - a textadventure done by Roman Schabus");
             Console.WriteLine("\n You are in a Castle. There are three different rooms each containing two puzzles to solve in order to reacht the next room\n");
-            string storytext = """
+
+            Console.WriteLine("""
                 You are a treasure hunter on a mission to find the Golden Key, an ancient artifact that unlocks a legendary treasure trove. 
                 Legend has it that the key is hidden deep within a labyrinthine castle, guarded by traps and puzzles. 
                 Your mission is to navigate through the castle, solve the puzzles, and find the Golden Key.
-                """;
-
-            Console.WriteLine(storytext);
+                """);
 
             Console.WriteLine("Good luck on your quest!");
 
         }
 
-        private static void PrintRoomDescription1()
+        private static void Room1()
+        {
+            bool keyFound = false;
+            PrintRoom1();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (!CheckInput(input)) 
+                {
+                    PrintError();
+                    continue;
+                }
+
+                if (input.StartsWith("u"))
+                {
+                    if (input.Contains("sword"))
+                    {
+                        Console.WriteLine("""
+                        The sword has been pulled and the helmet of the knight opens. 
+                        It contains a key. You take the key and put it in your pockets.
+                        """);
+
+                        keyFound = true;
+                    }
+                    else
+                    {
+                        PrintError();
+                    }
+                }
+                
+                if (keyFound && input == "e")
+                {
+                    Console.WriteLine("You find yourself looking at a locked door.");
+
+                    while(true)
+                    {
+                        input = Console.ReadLine();
+
+                        if (!CheckInput(input))
+                        {
+                            continue;
+                        }
+
+                        if (input.StartsWith("u") && input.Contains("key"))
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine("You open the door and walk into the other room.");
+        }
+
+        private static void PrintRoom1()
         {
             PrintFancyHeading("The Entrance Hall");
+
             Console.WriteLine("""
                 You enter the castle through a grand entrance hall. 
-                In the center of the room is a large statue of a knight, holding a sword.
-                To the left of the statue is a table with a locked chest on it. 
-                To the right is a door leading to the next room.
+                In the center of the room is a large statue of a knight, holding a sword that looks like it can be used.
+                To the right is a door leading to the next room. Find the key that opens the door.
                 """);
-
-            Console.WriteLine("""
-                Find the key to the chest. The key is hidden somewhere in the room. 
-                Once you unlock the chest, you find a piece of parchment with a riddle on it.
-                """);
-
         }
 
         private static void PrintRoomDescription2()
@@ -104,7 +157,20 @@
 
         private static void PrintFancyHeading(string text)
         {
-            Console.WriteLine($"################# {text} #################");
+            Console.WriteLine($"\n\n################# {text} #################\n\n");
+        }
+
+        private static bool CheckInput(string input)
+        {
+
+            string command = input[0].ToString();
+
+            if (!String.IsNullOrEmpty(input) && allowedCommands.Contains(command))
+            {
+                return true;
+            }
+
+            return false;
         }
         private static void PrintError()
         {
