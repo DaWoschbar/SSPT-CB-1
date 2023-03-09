3731822bacfd447f8270f45757e3b7736b1d3432
Roman Schabus <54069085+DaWoschbar@users.noreply.github.com>
2023-03-09 19:06:29 +0100

    finished delegates & schere stein papier

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+
+
+namespace Delegate
+{
+    internal class Program
+    {
+        //delcare delegate
+        private delegate bool DelegateSelection(string marticularnumber, DateTime date);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DateTime dt;
+                string marticulationNumber;
+
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Geben Sie das Datum ein (Format: TT.MM.YYYY):");
+                        string date = Console.ReadLine() ?? "";
+
+                        if (String.IsNullOrEmpty(date))
+                        {
+                            Console.WriteLine("Keine Eingabe");
+                            continue;
+                        }
+
+                        else if (!DateTime.TryParse(date, out dt))
+                        {
+                            Console.WriteLine("Ungültiges Datumsformat oder Datum");
+                            continue;
+                        }
+
+                        break;
+                    }
+
+                    while (true)
+                    { 
+                        Console.WriteLine("Geben Sie eine Martrikelnummer ein (Format isJJXXXX):");
+                        marticulationNumber = Console.ReadLine() ?? "";
+
+                        if (!CheckIfMarticulationNumberValid(marticulationNumber))
+                        {
+                            Console.WriteLine("Keine Eingabe");
+                            continue;
+                        }
+                        break;
+                    }
+
+                    PrintOptions();
+
+                    while (true) {
+                        string selection = Console.ReadLine() ?? "";
+
+                        DelegateSelection delsec = null;
+
+                        switch (selection)
+                        {
+                            case "1":
+                                delsec = new DelegateSelection(CheckMonth);
+                                break;
+
+                            case "2":
+                                delsec = new DelegateSelection(CheckDayOfMarticulationNumber);
+                                break;
+
+                            case "3":
+                                delsec = new DelegateSelection(MonthInMartriculationNumber);
+                                break;
+
+                            default:
+                                Console.WriteLine("Ungültige Eingabe");
+                                continue;
+                        }
+
+                        if (delsec(marticulationNumber, dt))
+                            PrintResult("JA");
+                        else
+                            PrintResult("NEIN");
+
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error\n" + ex.Message);
+                    continue;
+                }
+            }
+        }
+
+        private static void PrintOptions()
+        {
+            Console.WriteLine("""
+                1 ... Ist der Monat in der Matrikelnummer enthalten?
+                2 ... Ist der Tag in der Matrikelnummer enthalten? 
+                3 ... Ist das Jahr des Datums das Inskriptionsjahr der Matrikelnummer?
+                """);
+        }
+
+        private static bool CheckMonth(string marticularnumber, DateTime date)
+        {
+            if (marticularnumber.Contains(date.Month.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        private static bool CheckDayOfMarticulationNumber(string marticularnumber, DateTime date)
+        {
+            if (marticularnumber.Contains(date.Day.ToString()))
+                return true;
+            else
+                return false;
+        }
+
+        private static bool MonthInMartriculationNumber(string marticularnumber, DateTime date)
+        {
+            string year = date.Year.ToString();
+            if (marticularnumber[2] == year[0] && marticularnumber[3] == year[1])
+                return true;
+            else
+                return false;
+        }
+
+        private static bool CheckIfMarticulationNumberValid(string marticulationNumber)
+        {
+            if (String.IsNullOrEmpty(marticulationNumber) || marticulationNumber.Length < 7) 
+                return false;
+
+            return true;
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Ergebnis: {result}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ SchereSteinPapier/SchereSteinPapier/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace SchereSteinPapier
+{
+    internal class Program
+    {
+        //declare fields to count the won rounds 
+        private static int _playerWon = 0;
+        private static int _computerWon = 0;
+        private static int _ties = 0;
+        private static int _roundCounter = 0;
+
+        //List to determine the choosen type
+        private static Dictionary<int, string> _selection = new() { { 1, "Schere" }, { 2, "Stein" }, { 3, "Papier" } };
+
+        //Global accessible variable winner
+        private static string _winner = "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Schere Stein Papier");
+            while (true)
+            {
+                PrintOptions();
+                int playerSelection = GetPlayerSelection();
+
+                if (playerSelection == -1)
+                {
+                    Console.WriteLine("Ungültige Eingabe");
+                    continue;
+                }
+                else if (playerSelection == 0)
+                {
+                    Environment.Exit(0);
+                }
+
+                int computerSelection = GetComputerSelection();
+
+                DetermineWinner(playerSelection, computerSelection);
+                Console.Clear();
+                PrintResults(playerSelection, computerSelection);
+            }
+        }
+
+        private static void DetermineWinner(int player, int computer)
+        {
+            if (player == computer)
+            {
+                _ties++;
+            }
+            else if ((player == 1 && computer == 2) || (player == 2 && computer == 3) || (player == 3 && computer == 1))
+            {
+                _winner = "Computer";
+                _computerWon++;
+            }
+            else
+            {
+                _winner = "Player";
+                _playerWon++;
+            }
+            _roundCounter++;
+        }
+
+        private static int GetComputerSelection()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 4);
+        }
+
+        private static int GetPlayerSelection()
+        {
+            Console.WriteLine("Wählen Sie aus:");
+            string input = Console.ReadLine() ?? "";
+            int res = 0;
+
+            if (String.IsNullOrEmpty(input))
+                return -1;
+
+            else if(!int.TryParse(input, out res))
+                return -1;
+
+            if (res > 3 || res < 0)
+                return -1;
+
+            return res;
+        }
+
+        private static void PrintOptions()
+        {
+            Console.WriteLine("""
+                    1 Schere
+                    2 Stein
+                    3 Papier
+                    0 Beenden
+                """);
+        }
+
+        private static void PrintResults(int valuePlayer, int valueComputer)
+        {
+            string player = _selection[valuePlayer];
+            string computer = _selection[valueComputer];
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Round\t\t\t: {_roundCounter}");
+            Console.WriteLine($"Spieler\t\t\t: {player}");
+            Console.WriteLine($"Computer\t\t: {computer}");
+            Console.WriteLine($"Gewinner\t\t: {_winner}");
+            Console.WriteLine($"Spielstand\t\t: Player {_playerWon}\t Computer: {_computerWon}");
+            Console.WriteLine($"Unentschieden\t\t: {_ties}");
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
